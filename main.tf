65d254cf936aad7e06485fc04b9b40edb505d976
Gagan Gupta <89258969+gagangupta1404@users.noreply.github.com>
2023-06-05 15:02:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform { 
+  backend "azure" {} 
+  }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
